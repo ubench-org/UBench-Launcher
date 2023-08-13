7a9a4616abbe274879d26c8ec91ff6a897034a9a
ubench-org <ubench.org@gmail.com>
2023-08-13 16:55:11 +0200

    added toolbars

--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,7 @@ for (var i = 0; i < sysinfo.gpu.length; i++) {
 document.getElementById("system_content").innerHTML = `
     <p class="sirow"><b>${sysinfo.system.model}</b></p>
     <line/>
-    <p class="sirow"><img class="system-icon" src="./icons/grey/os.png"><b class="key">System</b> ${sysinfo.os.distro}</p>
+    <p class="sirow"><img class="system-icon" src="./icons/grey/os.png"><b class="key">System</b> ${sysinfo.os.distro} ${sysinfo.os.arch}</p>
     <line/>
     <p class="sirow"><img class="system-icon" src="./icons/grey/motherboard.png"><b class="key">Board</b> ${sysinfo.board.model}<br>Version ${sysinfo.board.version}<br>Made by ${sysinfo.board.manufacturer}</p>
     <line/>
@@ -22,7 +22,7 @@ document.getElementById("system_content").innerHTML = `
     <p class="sirow"><img class="system-icon" src="./icons/grey/ram.png"><b class="key">Memory</b> ${sysinfo.ram} GB</p>
     <line/>
     <p class="sirow"><img class="system-icon" src="./icons/grey/disk.png"><b class="key">Storage</b> ${disks}</p>
-
+    <line/>
     `;
 
 let url = "https://firestore.googleapis.com/v1/projects/ubench-75d3f/databases/(default)/documents/downloads"
@@ -97,14 +97,18 @@ for (var i = 0; i < navitems_bottom.length; i++) {
 }
 
 
+let v = window.localStorage.getItem("view_id") || "system";
+change_selected_menu_item(v);
+
 function change_selected_menu_item(id) {
+    window.localStorage.setItem("view_id",id);
     //var id = document.getElementById("selected_menu_item").innerHTML;
     var n = document.getElementsByClassName('nav-item');
     for (var i = 0; i < n.length; i++) {
         n[i].classList.remove('active');
     }
     document.getElementById(id).classList.add("active");
-    document.getElementById("title").innerHTML = id.toUpperCase();
+    document.getElementById("title").innerHTML = titleCase(id);
     document.getElementById("title_img").src = `./icons/${id}.svg`;
     show_view(id);
 }
@@ -114,6 +118,18 @@ function show_view(id) {
     for (var i = 0; i < n.length; i++) {
         n[i].hidden = true;
     }
+    var n = document.getElementsByClassName("toolbar_view");
+    for (var i = 0; i < n.length; i++) {
+        n[i].hidden = true;
+    }
 
     document.getElementById(`${id}_content`).hidden = false;
+    document.getElementById(`${id}_toolbar`).hidden = false;
 }
+function titleCase(str) {
+    str = str.toLowerCase().split(' ');
+    for (var i = 0; i < str.length; i++) {
+      str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
+    }
+    return str.join(' ');
+  }
